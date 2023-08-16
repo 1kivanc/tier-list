020d2c6b27061a1d72fc52e02b27393f5c8be272
Kıvanç Çoban <87445533+1kivanc@users.noreply.github.com>
2023-08-16 15:45:33 +0300

    Update rows.js

--- rows.js
+++ rows.js
@@ -1,19 +1,1 @@
-const rows = document.querySelectorAll('.row');
-const colors = ['green', 'aquamarine', 'yellow', 'orange', 'orangered', 'red'];
 
-const onDragOver = (event) => {
-  event.preventDefault();
-}
-
-const onDrop = (event) => {
-  event.preventDefault();
-  
-  console.log('dropped the element');
-}
-
-rows.forEach((row, index) => {
-  const label = row.querySelector('.label');
-  label.style.backgroundColor = colors[index];
-  row.ondragover = onDragOver;
-  row.ondrop = onDrop;
-})
